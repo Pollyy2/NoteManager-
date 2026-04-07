ec17da5abbd40e8836cb1986b9dd24a4324e7c63
Pollyy2 <polina.1998@abv.bg>
2026-04-07 22:04:39 +0300

    Fixed double test call

--- test_notes.py
+++ test_notes.py
@@ -1,12 +1,15 @@
 from db import add_note, delete_note, view_note
 
+note_id = ""
+
 def test_add_note():
+    global note_id
     note_id = add_note("Test Note", "This is a test note.")
     assert isinstance(note_id, int)
     return note_id
 
 def test_delete_note():
-    note_id = test_add_note()
+    global note_id
     delete_note(note_id)
     note = view_note(note_id)
     assert note is None
